77a8b4b0637a6c060fe999752cd2402ce89a9a3a
wchamber01 <williamchambers@gmail.com>
2020-07-17 01:43:51 -0400

    added card component and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component.jsx';
 
 class App extends Component {
   constructor() {
@@ -19,9 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}> {monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
